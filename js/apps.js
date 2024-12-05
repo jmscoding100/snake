da9f1b0e1a3bc7100c31922af1d0f2b8c9903842
jmscoding100 <joshua.z.mcgee@gmail.com>
2024-12-05 11:31:14 -0600

    snake-3

--- js/apps.js
+++ js/apps.js
@@ -27,15 +27,51 @@ class Game{
 
         this.foodX = 0
         this.foodY = 0
+
+        this.score = {
+            currScore: 0,
+            prevScore: 0,
+            hiScore: 0
+        }
     }
+
+
+    startGame(){
+        this.snake = [
+            {x: 200, y: 200},
+            {x: 190, y: 200},
+            {x: 180, y: 200},
+            {x: 170, y: 200},
+            {x: 160, y: 200}
+        ]
+
+        this.dx = 10
+        this.dy= 0
+
+        this.speed = 100
+
+        this.foodX = 0
+        this.foodY = 0
+        
+        this.score = {
+            currScore: 0,
+            prevScore: this.score.prevScore,
+            hiScore: this.score.hiScore
+        }
+
+        snake.init()
+        snake.generateFood()
+    }
+
+
+
 
     init(){
         // console.log('init')
         if(this.hasGameEnded()){
+            this.setPrevScore()
             return
         }
-
-
 
         this.changingDirection = false
         // make a timer
@@ -142,6 +178,11 @@ class Game{
         const hasEatenFood = snake[0].x === this.foodX && snake[0].y === this.foodY
 
         if(hasEatenFood){
+            this.score.currScore+= 10
+            this.setScores()
+            this.speed-= 5
+            const displayScore = document.getElementById('score')
+            displayScore.innerText = this.score.currScore
             this.generateFood()
         } else {
             snake.pop()
@@ -185,12 +226,42 @@ class Game{
             this.dy = 10
         }
     }
+
+    //9 set score
+    setScores(){
+        const hiScoreDisplay = document.getElementById('hiScore')
+
+        if(this.score.currScore > this.score.hiScore){
+            this.score.hiScore = this.score.currScore
+        }
+
+        hiScoreDisplay.innerText = this.score.hiScore 
+    }
+
+    setPrevScore(){
+        const prevScoreDisplay = document.getElementById('prevScore')
+
+        if(this.hasGameEnded()){
+            this.score.prevScore = this.score.currScore
+            prevScoreDisplay.innerText = this.score.prevScore
+        }
+    }
 }
 
 const snake = new Game()
+
+const gameBtn = document.getElementById('gameBtn')
+
+gameBtn.addEventListener('click', ()=> {
+    // console.log('click')
+    snake.startGame()
+})
+
 
 snake.init()
 
 document.addEventListener('keydown', ()=> {
     snake.changeDirection(event)
 })
+
+snake.generateFood()
